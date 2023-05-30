d9b715dfcea00564b638105bc490d097a0988fcc
temtemtemidoit <131671020+temtemtemidoit@users.noreply.github.com>
2023-05-30 17:28:13 +0800

    Create cubewithshading.py

---
+++ cubewithshading.py
@@ -0,0 +1,78 @@
+import pygame
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+def init():
+    pygame.init()
+    display = (800, 600)
+    pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+
+    gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
+    glTranslate(0.0, 0.0, -5)
+
+    glEnable(GL_DEPTH_TEST)
+    glEnable(GL_LIGHTING)
+    glEnable(GL_LIGHT0)
+    glLightfv(GL_LIGHT0, GL_POSITION, (1, 1, 1, 0))
+
+def draw_cube():
+    vertices = (
+        (1, -1, -1),
+        (1, 1, -1),
+        (-1, 1, -1),
+        (-1, -1, -1),
+        (1, -1, 1),
+        (1, 1, 1),
+        (-1, -1, 1),
+        (-1, 1, 1)
+    )
+
+    faces = (
+        (0, 1, 2, 3),
+        (3, 2, 7, 6),
+        (6, 7, 5, 4),
+        (4, 5, 1, 0),
+        (1, 5, 7, 2),
+        (4, 0, 3, 6)
+    )
+
+    normals = (
+        (0, 0, -1),
+        (0, 0, 1),
+        (0, 1, 0),
+        (0, -1, 0),
+        (1, 0, 0),
+        (-1, 0, 0)
+    )
+
+    glEnable(GL_BLEND)
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+
+    glBegin(GL_QUADS)
+    for face, normal in zip(faces, normals):
+        glNormal3fv(normal)
+        for vertex in face:
+            glVertex3fv(vertices[vertex])
+    glEnd()
+
+def main():
+    init()
+
+    clock = pygame.time.Clock()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        glRotatef(1, 3, 1, 1)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        draw_cube()
+        pygame.display.flip()
+        clock.tick(60)
+
+if __name__ == '__main__':
+    main()
+
